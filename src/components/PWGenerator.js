48a73593eb08efb6e2e7e2330f0874e580569062
Tamás <codesigner.hu@gmail.com>
2020-02-17 21:57:46 +0100

    checking password length

--- src/components/PWGenerator.js
+++ src/components/PWGenerator.js
@@ -9,15 +9,31 @@ class PWGenerator extends Component {
     containsSymbols: false
   };
 
+  handleChange = input => e => {
+    let targetValue = e.target.value;
+
+    this.setState({
+      passwordLength: targetValue.length > 7 ? true : false
+    });
+  };
+
   render() {
+    let { passwordLength } = this.state;
+
     return (
       <>
         <div className="title">Password Checker</div>
         <div className="content">
           <form action="">
-            <input type="text" placeholder="Please " />
+            <input
+              type="text"
+              placeholder="Please "
+              onChange={this.handleChange("password")}
+            />
             <div>
-              <div>Containes more than 8 character</div>
+              <div className={passwordLength ? "green" : null}>
+                Containes more than 8 character
+              </div>
               <div>Containes numbers</div>
               <div>Containes UpperCase</div>
               <div>Containes Symbols</div>
